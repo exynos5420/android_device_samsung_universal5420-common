74d2d446d22a75311a93ec26003ca0901f372f16
LuK1337 <priv.luk@gmail.com>
2018-01-10 03:10:44 +0100

    universal5420: Build libsecnativefeature from source
    Change-Id: I5effa7af2057ed50f70b09887a4c3414974b5b1a

--- device-common.mk
+++ device-common.mk
@@ -149,6 +149,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
 
+# SEC
+PRODUCT_PACKAGES += \
+    libsecnativefeature
+
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
