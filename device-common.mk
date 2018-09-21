17807425e5772041eb553e37f6581a28bb6ff95e
Stricted <info@stricted.net>
2018-09-22 00:16:54 +0200

    universal5420: build configstore HIDL HAL
    Change-Id: If2d406bff9a2d8ba57c2184e0075ea99204fb141

--- device-common.mk
+++ device-common.mk
@@ -40,6 +40,11 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0-impl \
     libbt-vendor
 
+# Configstore
+PRODUCT_PACKAGES += \
+    android.hardware.configstore@1.0-impl \
+    android.hardware.configstore@1.0-service
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
