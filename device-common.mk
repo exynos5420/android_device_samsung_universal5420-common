bb2cd21d8ed15aef87760cb4124f2e807e0ab409
Stricted <info@stricted.net>
2018-09-22 00:19:33 +0200

    universal5420: build renderscript hal
    Change-Id: I3699568130975c997063c51a29b67da972f39e80

--- device-common.mk
+++ device-common.mk
@@ -80,6 +80,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
+# RenderScript
+PRODUCT_PACKAGES += \
+    android.hardware.renderscript@1.0-impl
+
 # Wifi
 PRODUCT_PACKAGES += \
     wifiloader \
