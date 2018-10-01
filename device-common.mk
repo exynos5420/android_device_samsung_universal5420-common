02d4f3765c0578c12385492c6637e614a39e0784
Stricted <info@stricted.net>
2018-10-01 11:54:00 +0200

    universal5420: build sensor packages
    Change-Id: Iff1ceee2642219851e273f61529914a14a010d06

--- device-common.mk
+++ device-common.mk
@@ -126,6 +126,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
 
+# Sensors
+PRODUCT_PACKAGES += \
+    android.hardware.sensors@1.0-impl
+
 # Shims
 PRODUCT_PACKAGES += \
     libgutils
