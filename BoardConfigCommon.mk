d6b40be60156526b265061b3476f08d394f8b04a
Anton Hansson <hansson@google.com>
2019-02-08 17:56:34 +0000

    universal5420: Use generic arch variant
    Follow blueline which has not demonstrated any regressions
    after switching to a generic arch variant.
    
    Bug: 120773446
    Test: make
    Change-Id: I9e23963fcfa5ccaa912f2930a632458c4d2a692d

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -45,7 +45,7 @@ TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
-TARGET_CPU_VARIANT := cortex-a15
+TARGET_CPU_VARIANT := generic
 
 # Dexpreopt
 ifeq ($(HOST_OS),linux)
