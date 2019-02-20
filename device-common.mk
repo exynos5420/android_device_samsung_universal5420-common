f4c368538b94ea9303071350ce09320e4a9661f2
Sean Callanan <spyffe@google.com>
2019-02-20 12:45:08 -0800

    universal5420: Switch to IMapper 2.1
    Test: display CTS
    Bug: 62535446
    Change-Id: I2d6d0d118014eb2b99a6832b4c900ae21c062517

--- device-common.mk
+++ device-common.mk
@@ -93,7 +93,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.1-impl \
-    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.graphics.mapper@2.0-impl-2.1 \
     libhwc2on1adapter
 
 # Health
