4805649125bf96901eb1979eb58209aa53a1b0ad
Anton Hansson <hansson@google.com>
2019-02-21 15:04:20 +0000

    universal5420: Set cpu "runtime variant" to cortex-a15
    This makes bionic and various art tools that compile/runs cpu-specific
    code choose the krait variant at runtime. It does not affect
    build-time compilation.
    
    Bug: 120773446
    Test: bionic benchmarks with and without this change
    Change-Id: I670c8acd094090135515688c5f057c2cc08a083f

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -46,6 +46,7 @@ TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT_RUNTIME := cortex-a15
 
 # Dexpreopt
 ifeq ($(HOST_OS),linux)
