0fdd86c35ca3119fce5c3104abd9134b68f0b867
Erfan Abdi <erfangplus@gmail.com>
2019-09-04 17:20:32 +0430

    universal5420: Drop module eng tags
    Change-Id: I2e84d598b2412e617e5575bfdefd804612f76889

--- ramdisk/Android.mk
+++ ramdisk/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE            := fstab.universal5420
-LOCAL_MODULE_TAGS       := optional eng
+LOCAL_MODULE_TAGS       := optional
 LOCAL_MODULE_CLASS      := ETC
 LOCAL_SRC_FILES         := etc/fstab.universal5420
 LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
@@ -10,7 +10,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE		:= init.samsung.rc
-LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_TAGS	:= optional
 LOCAL_MODULE_CLASS	:= ETC
 LOCAL_SRC_FILES		:= etc/init.samsung.rc
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
@@ -18,7 +18,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE		:= init.universal5420.rc
-LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_TAGS	:= optional
 LOCAL_MODULE_CLASS	:= ETC
 LOCAL_SRC_FILES		:= etc/init.universal5420.rc
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
@@ -26,7 +26,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE        := init.recovery.universal5420.rc
-LOCAL_MODULE_TAGS   := optional eng
+LOCAL_MODULE_TAGS   := optional
 LOCAL_MODULE_CLASS  := ETC
 LOCAL_SRC_FILES     := etc/init.recovery.universal5420.rc
 LOCAL_MODULE_PATH   := $(TARGET_ROOT_OUT)
@@ -34,7 +34,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE		:= init.universal5420.usb.rc
-LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_TAGS	:= optional
 LOCAL_MODULE_CLASS	:= ETC
 LOCAL_SRC_FILES		:= etc/init.universal5420.usb.rc
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
@@ -42,7 +42,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE		:= init.universal5420.wifi.rc
-LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_TAGS	:= optional
 LOCAL_MODULE_CLASS	:= ETC
 LOCAL_SRC_FILES		:= etc/init.universal5420.wifi.rc
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
@@ -50,7 +50,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE		:= ueventd.universal5420.rc
-LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_TAGS	:= optional
 LOCAL_MODULE_CLASS	:= ETC
 LOCAL_SRC_FILES		:= etc/ueventd.universal5420.rc
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
