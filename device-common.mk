d1c82800badf99e34396f87a332e4681ba3306f2
LuK1337 <priv.luk@gmail.com>
2019-12-16 19:15:58 +0000

    universal5420: Build our MultiHAL sensors HAL
    * Move our sensors HAL out of `lib/hw` to `lib`.
    
    Change-Id: I32172a0a6f5b3bf15ead878e91c2edd25b6f1625

--- device-common.mk
+++ device-common.mk
@@ -128,7 +128,11 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-impl
+    android.hardware.sensors@1.0-impl \
+    sensors.exynos5
+
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/hardware/sensors/_hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/_hals.conf
 
 # Shims
 PRODUCT_PACKAGES += \
