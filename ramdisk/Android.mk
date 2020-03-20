0a43c9788f8ed24a27fce91ba46fe744ba1aeeb1
Mohamed Moussa <mmouss@codeaurora.org>
2020-03-19 19:54:46 -0700

    universal5420: ramdisk: Move init.recovery.universal5420.rc out of root
    The file init.recovery.universal5420.rc flagged as offending file in root/,
    because it violates the pathing req. (system/ & root/). Place the file
    in $(TARGET_RECOVERY_ROOT_OUT)
    
    Change-Id: Iefae195fec947127ffc33c013ebabca072ba81e3

--- ramdisk/Android.mk
+++ ramdisk/Android.mk
@@ -38,7 +38,7 @@ LOCAL_MODULE        := init.recovery.universal5420.rc
 LOCAL_MODULE_TAGS   := optional
 LOCAL_MODULE_CLASS  := ETC
 LOCAL_SRC_FILES     := etc/init.recovery.universal5420.rc
-LOCAL_MODULE_PATH   := $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH   := $(TARGET_RECOVERY_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
