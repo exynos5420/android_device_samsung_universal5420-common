e55debd52abfa817907d817ac30f202e2c71e643
Francescodario Cuzzocrea <bosconovic@gmail.com>
2020-08-28 09:27:26 +0200

    universal5420: power: rebrand to universal5420
    Change-Id: I35b0c08e087e34f09367bd523e54bfad4f1934a9

--- device-common.mk
+++ device-common.mk
@@ -131,7 +131,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-service.exynos
+    android.hardware.power@1.0-service.universal5420
 
 # Permissions
 PRODUCT_COPY_FILES += \
