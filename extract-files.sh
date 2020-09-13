36be32364596f1dab6e5893d9d5795f47593dfbf
dianlujitao <dianlujitao@lineageos.org>
2020-09-13 16:18:28 +0800

    universal5420: Add prebuilt protobuf from sdk29
    This is needed for our prebuilt DRM and Mobicore libraries.
    
    Change-Id: I980a9e66cc0f99b5292210428c6ae9737e5b3969

--- extract-files.sh
+++ extract-files.sh
@@ -71,4 +71,7 @@ BLOB_ROOT="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE_COMMON"/proprietary
 # Update trustlets location
 sed -i 's|system/app|vendor/app|g' $BLOB_ROOT/vendor/bin/mcDriverDaemon
 
+# Replace protobuf with vndk29 compat libs for specified libs
+patchelf --replace-needed libprotobuf-cpp-lite.so libprotobuf-cpp-lite-v29.so $BLOB_ROOT/vendor/lib/libMcClient.so
+
 "${MY_DIR}"/setup-makefiles.sh
