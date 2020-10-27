5ae2590dc6795e22f83f20b29c49672ad58de0ab
Arne Coucheron <arco68@gmail.com>
2020-10-27 06:18:47 +0100

    universal5420: Use XZ compressed ramdisks
       * Old LZMA_RAMDISK_TARGETS is deprecated.
    
    Change-Id: I3b9205500bb1777ff0f7caa5a6a61fcacdeb6ad6

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -112,7 +112,7 @@ TARGET_POWERHAL_VARIANT := samsung
 # Recovery
 BOARD_RECOVERY_SWIPE := true
 TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/ramdisk/etc/fstab.universal5420
-LZMA_RAMDISK_TARGETS := recovery
+BOARD_RAMDISK_USE_XZ := true
 
 # Samsung  OpenMAX Video
 BOARD_USE_STOREMETADATA := true
