0a954b6f23688dacf4396a78c6a73fc63c009fbb
Francescodario Cuzzocrea <bosconovic@gmail.com>
2021-02-28 11:12:33 +0100

    universal5420: add common sepolicy
        * also remove creation of useless dirs from init scripts to avoid
          hassles with sepolicy writing

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -150,6 +150,7 @@ include device/lineage/sepolicy/exynos/sepolicy.mk
 BOARD_SEPOLICY_TEE_FLAVOR := mobicore
 include device/samsung_slsi/sepolicy/sepolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
 # For legacy HAL1 camera
 SELINUX_IGNORE_NEVERALLOWS := true
 
