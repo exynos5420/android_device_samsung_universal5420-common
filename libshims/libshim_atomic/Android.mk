a920beddfeda29b6a7a74839cd3e2a37ecc53e2a
ShevT <evgeny.shishkov@gmail.com>
2021-11-25 14:18:45 +0300

    universal5420: Add atomic shim
    F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/gpsd": cannot locate symbol "android_atomic_release_cas" referenced by "/system/vendor/lib/libsec-ril.so"...
    
    Change-Id: Ia256bf38b1330ef4f069e8f26e43948bfcf597e8

---
+++ libshims/libshim_atomic/Android.mk
@@ -0,0 +1,9 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := atomic.cpp
+LOCAL_MODULE := libshim_atomic
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_VENDOR_MODULE := true
+include $(BUILD_SHARED_LIBRARY)
