11c5a6de2a7bb8e3a8c19e7f17c62694344810ed
Ian Hope <ian.hope666@gmail.com>
2022-10-22 17:14:20 +1100

    universal5420-common: Fix compiler warning about commenting blocks
    Change-Id: I9f99e5939e651ce6959c45db12d3b6a06fe51ade

--- libshims/libgutils/popcountsi2.c
+++ libshims/libgutils/popcountsi2.c
@@ -23,7 +23,7 @@ int __popcountsi2(int a)
     x = (x + (x >> 4)) & 0x0F0F0F0F;
     /* Every 8 bits holds the sum of every 8-set of bits (4 significant bits) */
     x = (x + (x >> 16));
-    /* The lower 16 bits hold two 8 bit sums (5 significant bits). 
+    /* The lower 16 bits hold two 8 bit sums (5 significant bits). */
     /*    Upper 16 bits are garbage */
     return (x + (x >> 8)) & 0x0000003F;  /* (6 significant bits) */
 }
@@ -48,7 +48,7 @@ di_int __aeabi_d2lz(double a)
     return __fixunsdfdi(a);
 }
 
-/* for testing 
+/* for testing
 int main()
 {
     double x = 6434243343.141592653589793238;
